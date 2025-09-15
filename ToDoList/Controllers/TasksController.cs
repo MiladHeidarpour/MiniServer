0e5d825ac43a7928df077220efb062ad22cf3a78
Milad Heidarpour <mld.heidarpour@gmail.com>
2025-09-15 11:06:41 +0330

    feat: Add JSON serialization and in-memory data
    This commit enables the framework to serve structured data, moving from plain text responses to the web standard JSON format.

--- ToDoList/Controllers/TasksController.cs
+++ ToDoList/Controllers/TasksController.cs
@@ -1,22 +1,31 @@
 ﻿using MiniServer.Core.Attributes;
 using MiniServer.Core.Http;
 using MiniServer.Core.Routing;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers;
 
 public class TasksController : BaseController
 {
+    private static readonly List<TodoTask> _tasks = new()
+    {
+        new TodoTask { Id = 1, Title = "Learn about HTTP", IsCompleted = true },
+        new TodoTask { Id = 2, Title = "Build a Router", IsCompleted = true },
+        new TodoTask { Id = 3, Title = "Return JSON data", IsCompleted = false }
+    };
+
     [Route("GET", "/tasks")]
     public Task GetAllTasks()
     {
-        HttpContext.Response.Write("This will be a list of all tasks!");
+        HttpContext.Response.WriteJson(_tasks);
         return Task.CompletedTask;
     }
 
     [Route("GET", "/tasks/1")]
     public Task GetTaskById()
     {
-        HttpContext.Response.Write("This will be task with ID 1.");
+        var task = _tasks.FirstOrDefault(t => t.Id == 1);
+        HttpContext.Response.WriteJson(task);
         return Task.CompletedTask;
     }
 }
